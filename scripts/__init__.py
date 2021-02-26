4f4b80bd50255a960afbcc6636b29b3f11da21f1
Harsh Rajmachikar <harshavardhan@rajmachikar.com>
2021-02-26 17:16:34 +0530

    Delete scripts directory

--- scripts/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import _imports
-from . import advert_scraper
-from . import link_scraper
-from . import post_ad_form_scraper
--- scripts/_imports.py
+++
@@ -1,3 +0,0 @@
-from urllib.parse import urljoin
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup
